0e7e440cb5e8ab7034225688b9511cb936f01416
sullis <github@seansullivan.com>
2024-09-28 20:21:52 -0700

    wip InMemoryCatalogTest

--- src/test/java/io/github/sullis/iceberg/playground/InMemoryCatalogTest.java
+++ src/test/java/io/github/sullis/iceberg/playground/InMemoryCatalogTest.java
@@ -1,7 +1,11 @@
 package io.github.sullis.iceberg.playground;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
+import org.apache.commons.io.IOUtils;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.DataFiles;
 import org.apache.iceberg.PartitionSpec;
@@ -11,18 +15,23 @@ import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.inmemory.InMemoryFileIO;
 import org.apache.iceberg.inmemory.InMemoryCatalog;
+import org.apache.iceberg.io.FileIO;
+import org.apache.iceberg.io.InputFile;
+import org.apache.iceberg.io.OutputFile;
+import org.apache.iceberg.io.PositionOutputStream;
+import org.apache.iceberg.io.SeekableInputStream;
 import org.apache.iceberg.types.Types;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 public class InMemoryCatalogTest {
 
   private InMemoryCatalog catalog;
 
   @Test
-  public void happyPath() {
+  public void happyPath()
+      throws IOException {
     Namespace namespace = Namespace.of("test" + System.currentTimeMillis());
     String catalogName = "catname" + System.currentTimeMillis();
     catalog = new InMemoryCatalog();
@@ -50,5 +59,20 @@ public class InMemoryCatalogTest {
 
     assertThat(catalog.namespaceExists(namespace)).isTrue();
     assertThat(catalog.listNamespaces()).hasSize(1);
+
+    Table loadTableResult = catalog.loadTable(tableId);
+    FileIO io = loadTableResult.io();
+    assertThat(io).isInstanceOf(InMemoryFileIO.class);
+    String outputPath = "test-path-" + UUID.randomUUID();
+    OutputFile outputFile = io.newOutputFile(outputPath);
+    PositionOutputStream pos = outputFile.create();
+    pos.write("Hello".getBytes(StandardCharsets.UTF_8));
+    pos.flush();
+    pos.close();
+    InputFile inputFile = outputFile.toInputFile();
+    assertThat(inputFile.exists()).isTrue();
+    SeekableInputStream inputStream = inputFile.newStream();
+    assertThat(inputStream).asString(StandardCharsets.UTF_8)
+        .isEqualTo("Hello");
   }
 }
